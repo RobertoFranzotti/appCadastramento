4ae55db7b944e6b5bd04789f8562658a78371a80
robertofranzotti <robertofranzotti5@gmail.com>
2023-08-26 08:36:16 -0300

    Finalizado

--- App.js
+++ App.js
@@ -1,11 +1,97 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  TouchableOpacity,
+  FlatList,
+  Alert,
+} from "react-native";
+import Feather from "@expo/vector-icons/Feather";
 
 export default function App() {
+  const [name, setName] = useState("");
+  const [names, setNames] = useState([]);
+
+  function handleAddNewName() {
+    setNames((prevState) => [...prevState, name]);
+  }
+  function handleRemoveName(nameToRemove) {
+    Alert.alert(
+        "Remover",
+        `Remover o ${nameToRemove}?`,
+        [
+            {
+                text: "Não",
+                style: "cancel"
+            },
+            { 
+                text: "Sim", 
+                onPress: () => {
+                    const updatedNames = names.filter(name => name !== nameToRemove);
+                    setNames(updatedNames);
+                }
+            }
+        ],
+        { cancelable: false } 
+    );
+}
+    
+    
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View>
+        <Text style={styles.title}>Nome do evento</Text>
+        <Text style={styles.subtitle}>Sexta, 4 de Novembro de 2022.</Text>
+      </View>
+      <View style={styles.form}>
+        <TextInput
+          style={styles.input}
+          placeholder="Nome do participante"
+          placeholderTextColor="#6B6B6B"
+          onChangeText={(text) => setName(text)}
+          value={name}
+        />
+        <TouchableOpacity style={styles.button} onPress={handleAddNewName}>
+          <Feather name="plus-circle" size={24} color="#FFFFFF" />
+        </TouchableOpacity>
+      </View>
+      <View style={styles.part}>
+        <FlatList
+          data={names}
+          ListEmptyComponent={() => (
+            <View>
+              <Text style={styles.part1}>
+                Ninguém chegou no evento ainda? Adicione participantes a sua
+                lista de presença.
+              </Text>
+            </View>
+          )}
+          ListHeaderComponent={() => (
+            <View style={styles.header}>
+              <Text style={styles.part2}>Participantes</Text>
+            </View>
+          )}
+          renderItem={({ item }) => (
+           <> 
+            <View style={styles.card}>
+              <Text style={styles.cardText}>{item}</Text>
+              <TouchableOpacity
+                style={styles.button2}
+                onPress={() => handleRemoveName(item)}
+              >
+                <Feather name="minus-circle" size={24} color="#FFFFFF" />
+              </TouchableOpacity>
+            </View>
+
+              
+           </>
+            
+          )}
+        />
+      </View>
     </View>
   );
 }
@@ -13,8 +99,85 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#131016",
+    paddingHorizontal: 24,
+    paddingVertical: 75,
+  },
+  title: {
+    color: "#fdfcfe",
+    textAlign: "left",
+    fontSize: 24,
+  },
+  subtitle: {
+    color: "#6b6b6b",
+    fontSize: 16,
+  },
+  input: {
+    flex: 1,
+    paddingHorizontal: 17,
+    paddingVertical: 18,
+    fontSize: 14,
+    backgroundColor: "#1f1e25",
+    color: "#ffff",
+    borderRadius: 4,
+  },
+
+  form: {
+    marginTop: 34,
+    flexDirection: "row",
+    gap: 7,
+    alignItems: "center",
+  },
+
+  button: {
+    backgroundColor: "#31CF67",
+    padding: 14,
+    borderRadius: 4,
+  },
+
+  button2: {
+    backgroundColor: "#E23C44",
+    padding: 18,
+    borderRadius: 4,
+    right: -20,    
+    
+  },
+
+  part: {
+    marginTop: 44,
+  },
+
+  part1: {
+    color: "#FDFCFE",
+    fontSize: 14,
+    textAlign: "center",
+    marginTop: 28,
+  },
+
+  part2: {
+    color: "#FDFCFE",
+    fontSize: 20,
+  },
+
+  header: {
+    marginBottom: 20,
+  },
+
+  card: {
+    paddingHorizontal: 20,
+    backgroundColor: "#1F1E25",
+    marginBottom: 2,
+    justifyContent: "space-between", 
+    alignItems: "center", 
+    marginTop: 16,
+    flexDirection: "row",
+    
+   
+  },
+ 
+  cardText: {
+    color: "#FDFCFE",
+    fontSize: 16,
+
   },
 });
